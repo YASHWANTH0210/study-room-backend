31fcbee889adbf52aabb8c97043deb6b79c584b1
YASHWANTH0210 <satyarajeswari66@gmail.com>
2026-09-18 20:12:02 +0530

    Set up Express server with Socket.IO and CORS

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data.roomCode);
+    console.log(`User ${data.username} joined room: ${data.roomCode}`);
+  });
+
+  socket.on('send_message', (data) => {
+    io.to(data.roomCode).emit('receive_message', data);
+  });
+
+  socket.on('update_note', (data) => {
+    socket.to(data.roomCode).emit('receive_note', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
